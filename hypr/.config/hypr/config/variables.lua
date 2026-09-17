a330ad0ad60dbb43994d5844e7472f55e2e1f999
Lises <unnisemai@gmail.com>
2026-09-17 12:51:27 -0300

    Change default browser to zen-browser

--- hypr/.config/hypr/config/variables.lua
+++ hypr/.config/hypr/config/variables.lua
@@ -2,7 +2,7 @@
 
 TERMINAL     = "kitty"
 FILE_MANAGER = "dolphin"
-BROWSER      = "firefox"
+BROWSER      = "zen-browser"
 EDITOR       = "gnome-text-editor --new-window"
 CALCULATOR   = "gnome-calculator"
 
