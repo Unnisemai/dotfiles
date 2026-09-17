5189f0ae64b0e47b629f9835ba9a2580860ae778
Lises <unnisemai@gmail.com>
2026-09-17 12:31:00 -0300

    Add startup commands for dunst, waybar, and others

--- hypr/.config/hypr/config/autostart.lua
+++ hypr/.config/hypr/config/autostart.lua
@@ -3,6 +3,11 @@
 
 hl.on("hyprland.start", function ()
     hl.exec_cmd("dbus-update-activation-environment --systemd --all")
-    hl.exec_cmd("noctalia")
+    hl.exec_cmd("dunst")
+    hl.exec_cmd("waybar")
+    hl.exec_cmd("hyprpaper")
+    hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+    hl.exec_cmd("wl-paste --type text --watch cliphist store")
+    hl.exec_cmd("wl-paste --type image --watch cliphist store")
     hl.exec_cmd("xhost +SI:localuser:root")
 end)
